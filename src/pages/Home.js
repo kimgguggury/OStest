a00403b1a3e43d3cac89761c59c9c61a1db23383
김동현 <kimgguggury1002@gmail.com>
2024-08-26 18:18:08 +0900

    홈 부분 수평 수직 중앙으로 움직임

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,18 @@ import HomeCss from "../css/Home.module.css";
 function Home() {
   let navigate = useNavigate();
   return (
-    <div className={HomeCss.container}>
-      <div className={HomeCss.profile}>
-        <div className= {HomeCss.photo} />
-        <h1>유저님<br />스마트팜</h1>
-        
-      </div>
-      <div className={HomeCss.content}>
-        <div className={HomeCss.box}><h1>CCTV</h1></div>
-        <div className={HomeCss.box}><h1>온도/습도/토양</h1></div>
-        <div className={HomeCss.box}><h1>건강리포트</h1></div>
-        <div className={HomeCss.box} onClick={()=>{ navigate('/community') }}><h1>게시판</h1></div>
+    <div style={{display:"flex",justifyContent:"center",alignContent:"center"}}>
+      <div className={HomeCss.container}>
+        <div className={HomeCss.profile}>
+          <div className= {HomeCss.photo} />
+          <h1>유저님<br />스마트팜</h1>
+        </div>
+        <div className={HomeCss.content}>
+          <div className={HomeCss.box}><h1>CCTV</h1></div>
+          <div className={HomeCss.box}><h1>온도/습도/토양</h1></div>
+          <div className={HomeCss.box}><h1>건강리포트</h1></div>
+          <div className={HomeCss.box} onClick={()=>{ navigate('/community') }}><h1>게시판</h1></div>
+        </div>
       </div>
     </div>
   )
